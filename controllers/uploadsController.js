dde0f03d51abf34bf3b561de79321f36fee520a7
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-01-08 14:27:03 -0500

    image upload

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,18 @@
 const { StatusCodes } = require("http-status-codes");
+const path = require("path");
 
 const uploadProductImage = async (req, res) => {
-  console.log(req);
-  res.send("upload product");
+  const productImage = req.files.image;
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = {
