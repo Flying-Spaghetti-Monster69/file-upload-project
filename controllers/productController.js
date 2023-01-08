7115956b3b87804dc9ddae0e8629455dcb8d203b
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-01-08 14:36:44 -0500

    get all products logic added

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,13 +2,13 @@ const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 
 const createProducts = async (req, res) => {
-  console.log(req.body);
   const product = await Product.create(req.body);
   res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getAllProducts = async (req, res) => {
-  res.send("get all products products");
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
 };
 
 module.exports = {
