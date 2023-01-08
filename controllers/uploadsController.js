2f14fd10eb2fab8cd8079d5c4c7e1f91dde2acf7
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-01-08 15:29:40 -0500

    cloudinary update

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,10 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
 const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
   if (!req.files) {
     throw new CustomError.BadRequestError("no file uploaded");
   }
@@ -32,6 +34,16 @@ const uploadProductImage = async (req, res) => {
     .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    { use_filename: true, folder: "file-upload" }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+};
+
 module.exports = {
+  uploadProductImageLocal,
   uploadProductImage,
 };
