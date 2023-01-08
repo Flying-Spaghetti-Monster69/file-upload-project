60bfb5ac9d185a0d5622cdea41e77b67bad1e291
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-01-08 14:45:43 -0500

    upload errors added

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,25 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("no file uploaded");
+  }
+
   const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("please upload an image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "please upload an image smaller than 1mb"
+    );
+  }
 
   const imagePath = path.join(
     __dirname,
